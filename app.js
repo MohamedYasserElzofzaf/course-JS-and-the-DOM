6883ee65a8cf39a2aaa6b5a71279a40c002c5bfc
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-12 00:33:46 +0200

    still on event listener

--- app.js
+++ app.js
@@ -17,11 +17,25 @@
 
 // this will not work
 // adds a listener for clicks, to run the `myEventListeningFunction` function
-document.addEventListener("click", function myEventListeningFunction() {
-    console.log("howdy");
-});
+// document.addEventListener("click", function myEventListeningFunction() {
+//     console.log("howdy");
+// });
 
-// immediately removes the click listener that should run the `myEventListeningFunction` function
-document.removeEventListener("click", function myEventListeningFunction() {
-    console.log("howdy");
+// // immediately removes the click listener that should run the `myEventListeningFunction` function
+// document.removeEventListener("click", function myEventListeningFunction() {
+//     console.log("howdy");
+// });
+
+// document.addEventListener("click", function() {
+//     console.log("The document was clicked");
+// });
+
+// document.body.addEventListener("click", function() {
+//     console.log("The document body was clicked");
+// });
+
+// add event prameter
+document.addEventListener("click", function(event) {
+    // ← the `event` parameter is new!
+    console.log("The document was clicked");
 });
