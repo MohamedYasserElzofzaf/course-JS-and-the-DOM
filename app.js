89816a124ac7c919ab4c554c711fdac86d6b4fa4
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-03 14:14:11 +0200

    Add the clinet & server side to see how it work

--- app.js
+++ app.js
@@ -40,10 +40,32 @@
 //     console.log(event);
 // });
 
-const links = document.querySelectorAll("a");
-const thirdLink = links[2];
+// const links = document.querySelectorAll("a");
+// const thirdLink = links[2];
 
-thirdLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("Look, ma! We didn't navigate to a new page!");
-});
+// thirdLink.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     console.log("Look, ma! We didn't navigate to a new page!");
+// });
+
+let count = 1
+
+function generateParagraphs() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 1; i <= 500; i++) {
+        const newElement = document.createElement('p');
+        newElement.textContent = 'This is paragraph number ' + count;
+        count = count + 1;
+
+        fragment.appendChild(newElement);
+    }
+
+    document.body.appendChild(fragment);
+
+    if (count < 20000) {
+        setTimeout(generateParagraphs, 0);
+    }
+}
+
+generateParagraphs();
